0d2e9aafec1b54c46be070ba108f2c6bcdc31363
tristanjlang <tristan@alum.mit.edu>
2014-12-31 18:12:07 -0500

    updates for new data

--- tx-to-block.js
+++ tx-to-block.js
@@ -11,8 +11,8 @@ var query = { '$and': [ { 'height': { '$gt': minHeight } },
 
 
 mongodb.MongoClient.connect(uri, function(err, db) {
-  if (err) throw err;
-  fs.writeFile('output.txt', 'txHash,blockHash,prevBlock,mrklRoot,time,bits,nonce,height,numTx,size\n', function(err) { if (err) throw err; });
+  if (err) console.error(err);
+  console.log('txHash,blockHash,prevBlock,mrklRoot,time,bits,nonce,height,numTx,size');
 
   var output, id, txes, prev_block, mrkl_root, time, bits, nonce, height, n_tx, size;
   var stream = db.collection('data').find(query).stream();
@@ -33,7 +33,7 @@ mongodb.MongoClient.connect(uri, function(err, db) {
 
     for(var i=0; i < txes.length; i++) {
       output = [txes[i], id, prev_block, mrkl_root, time, bits, nonce, height, n_tx, size];
-      fs.appendFile('output.txt', output.join(',')+'\n', function(err) { if (err) throw err; });
+      console.log(output.join(','));
     }
 
     console.log('done with block', id, 'for time', time, 'at time', Date.now());
