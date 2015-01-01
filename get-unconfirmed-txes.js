b58639b505f74c7048d41073dcc463b71b017583
tristanjlang <tristan@alum.mit.edu>
2014-12-31 19:11:56 -0500

    update to read file line by line synchronously

--- get-unconfirmed-txes.js
+++ get-unconfirmed-txes.js
@@ -1,30 +1,29 @@
 /*
-  From stdin, takes an input file that consists of transactions.
+  Takes an input file as parameter that consists of transactions.
   Prints to stdout a list of unconfirmed transactions.
 */
 
 var mongodb = require('mongodb');
 var readline = require('readline');
-
-
+var LineByLineReader = require('line-by-line'),
+    lr = new LineByLineReader(process.argv[2]);
 var uri = 'mongodb://localhost:27017/bigchain';
-var query;
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
+
+
+lr.on('error', function (err) {
+  throw err;
 });
 
 
-rl.on('line', function (line) {
-  mongodb.MongoClient.connect(uri, function (err, db) {
-    if (err) console.error(err);
+mongodb.MongoClient.connect(uri, function (err, db) {
+  if (err) console.error(err);
+
+  lr.on('line', function (line) {
     if (line.length != 0) {
       processLine(line, db);
     }
   });
 });
-
 
 function processLine(transaction, db) {
   var collection = db.collection('data');
@@ -32,7 +31,6 @@ function processLine(transaction, db) {
     cursor.toArray(function (err, results) {
       if (err) throw err;
       if (results.length == 0) console.log(transaction);
-      db.close();
     });
   });
 }
